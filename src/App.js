105c6c8c4005f30281e1ac10ecbd167d436f60ac
unknown <you@example.com>
2018-07-28 22:57:11 +0200

    Add Redux and react-router dependancies

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Welcome to React</h1>
-        </header>
-        <p >
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Its OK !!!</h1>
       </div>
     );
   }
