12d733295efd0276bfe490446e015c9f576a6bcf
unknown <you@example.com>
2018-07-29 00:52:40 +0200

    Update style

--- src/App/App.js
+++ src/App/App.js
@@ -7,6 +7,7 @@ import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
+import "../style/style.css";
 
 class App extends React.Component {
     constructor(props) {
